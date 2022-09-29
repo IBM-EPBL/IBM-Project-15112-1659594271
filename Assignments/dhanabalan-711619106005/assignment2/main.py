8e5700b1911589eec6004ebcd33e1631f6ccf7eb
dhanabalan <dana7010276463@gmail.com>
2022-09-29 14:43:53 +0530

    Flask app

---
+++ Assignments/dhanabalan-711619106005/assignment2/main.py
@@ -0,0 +1,3 @@
+import ibm_db
+conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud; PORT=30376;SECURITY=SSL;SSlServerCertificate=DigiCertGlobalRootCA (1).crt;UID=lkc48002;PWD=wwunbEPZ83GLvVqb;", '', '')
+
